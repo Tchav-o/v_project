3d0811f9ece51d26859bb1959612b4ad4419796b
Tchav-o <69750517+Tchav-o@users.noreply.github.com>
2026-04-24 09:15:03 +0300

    Update View.php

--- core/View.php
+++ core/View.php
@@ -3,7 +3,10 @@ namespace Core;
 final class View{
   public static function make(string $v,array $d=[]):string{
     $file=dirname(__DIR__)."/resources/views/$v.php";
-    extract($d,EXTR_SKIP);ob_start();include$file;return ob_get_clean();
+    extract($d,EXTR_SKIP);
+    ob_start();
+    include$file;
+    return ob_get_clean();
   }
   public static function layout(string $l,array $d):string{
     return self::make("layouts/$l",$d);
